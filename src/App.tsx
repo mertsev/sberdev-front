77e8971be9424b5f50aa39f68d11a11c40d017e9
Mertsev Sergey <Mertsev.S.E@sberbank.ru>
2021-02-28 05:13:49 +0300

    massive cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,6 @@ import React from "react";
 import "./App.css";
 import { HeaderComponent } from "./features/header/Header";
 import { Route } from "react-router-dom";
-import { ApplianceList } from "./features/applianceList/ApplianceList";
-import { Appliance } from "./features/appliance/Appliance";
-import { ApplianceForm } from "./features/applianceForm/ApplianceForm";
-import { Job } from "./features/job/Job";
-import { JobList } from "./features/jobList/JobList";
-import { JobForm } from "./features/jobForm/JobForm";
-import { HomePage } from "./features/homePage/homePage";
 import { toDo } from "./features/toDo/toDo";
 import { AssistantComponent } from "./features/assistant/assistant";
 
@@ -60,19 +53,6 @@ function App(): JSX.Element {
         {/* <Route path="/" exact={true} component={toDo} /> */}
         <Route path="/" exact={true} component={foodCard} />
         <Route path="/recipe" exact={true} component={recipePage} />
-        {/* <Route path="/" exact={true} component={HomePage} /> */}
-        {/* <Route path="/appliance/view/:id" exact={true} component={Appliance} />
-      <Route path="/appliance/create/" exact={true} component={ApplianceForm} />
-      <Route
-        path="/appliance/edit/:id"
-        exact={true}
-        component={ApplianceForm}
-      />
-      <Route path="/job/view/:id" exact={true} component={Job} />
-      <Route path="/jobs" exact={true} component={JobList} />
-      <Route path="/appliance/jobs/:id" exact={true} component={JobList} />
-      <Route path="/job/create/" exact={true} component={JobForm} />
-      <Route path="/job/edit/:id" exact={true} component={JobForm} /> */}
       </div>
     </AppStyled>
   );
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,21 +1,9 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
-import applianceReducer from "../features/appliance/applianceSlice";
-import applianceListReducer from "../features/applianceList/applianceListSlice";
-import applianceFormReducer from "../features/applianceForm/applianceFormSlice";
-import jobReducer from "../features/job/jobSlice";
-import jobListReducer from "../features/jobList/jobListSlice";
 import toDoReducer from "../features/toDo/toDoSlice";
 import foodCardReducer from "../features/foodCard/foodCardSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    appliance: applianceReducer,
-    applianceList: applianceListReducer,
-    appliianceForm: applianceFormReducer,
-    job: jobReducer,
-    jobList: jobListReducer,
     toDoList: toDoReducer,
     foodCard: foodCardReducer,
   },
